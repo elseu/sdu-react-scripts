c38479721db1ca4f4d351f1f0f7c67cf1048b552
Daphne Smit <daphnesmit1986@gmail.com>
2021-03-23 12:21:14 +0100

    fix: add dependencies as resolve

--- config/rollup/index.js
+++ config/rollup/index.js
@@ -1,9 +1,9 @@
-const commonjs = require.resolve('@rollup/plugin-commonjs');
-const json = require.resolve('@rollup/plugin-json');
-const resolve = require.resolve('@rollup/plugin-node-resolve');
-const typescriptPlugin = require.resolve('@rollup/plugin-typescript');
-const url = require.resolve('@rollup/plugin-url');
-const externals = require.resolve('rollup-plugin-node-externals');
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
+const resolve = require('@rollup/plugin-node-resolve');
+const typescriptPlugin = require('@rollup/plugin-typescript');
+const url = require('@rollup/plugin-url');
+const externals = require('rollup-plugin-node-externals');
 
 const dirname = (file) => {
   const [root, folder] = file.split('/');
