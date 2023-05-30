8837c3701d3486f0e1d0cb5d4b3dfaede6347c1b
Daphne Smit <daphnesmit1986@gmail.com>
2023-05-30 12:32:08 +0200

    fix: turn off selector-anb-no-unmatchable

--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -13,6 +13,7 @@ const stylelintConfig = {
     'declaration-empty-line-before': null,
     'keyframes-name-pattern': null,
     'no-descending-specificity': null,
+    'selector-anb-no-unmatchable': null,
     'no-eol-whitespace': null,
     'selector-class-pattern': null,
     'string-quotes': null,
