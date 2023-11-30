25c2b2ee24b6db6ddeb965754413a5c0fc15d909
Tim de Wolf <tpdewolf@gmail.com>
2023-11-30 11:00:03 +0100

    add types

--- packages/i18n/README.md
+++ packages/i18n/README.md
@@ -11,10 +11,18 @@ npm i @lingui/cli @lingui/core @lingui/macro @lingui/react -D
 Create a `lingui.config.js` file in the root of your project
 
 ```typescript
-import config from '@elseu/sdu-react-scripts-i18n';
+import { config } from '@elseu/sdu-react-scripts-i18n';
 
 /** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
   ...config,
 };
+```
+
+Create npm scripts in `package.json`
+
+```
+"translations:extract": "lingui extract",
+"translations:compile": "lingui compile",
+"translations": "npm run translations:extract && npm run translations:compile"
 ```
--- packages/i18n/index.js
+++ packages/i18n/index.js
@@ -1,5 +1,13 @@
+/**
+ * Array of locale strings.
+ * @type {string[]}
+ */
 const locales = ['fr-FR', 'nl-NL', 'en-GB', 'es-ES', 'it-IT', 'de-DE', 'nl-BE', 'fr-BE', 'dev'];
 
+/**
+ * List of locale objects with title and value properties.
+ * @type {Array<{title: string, value: string}>}
+ */
 const localeList = [
   {
     title: 'Nederlands (Nederland)',
@@ -39,21 +47,31 @@ const localeList = [
   },
 ];
 
-/** @type {import('@lingui/conf').LinguiConfig} */
-module.exports = {
-  locales,
-  localeList,
-  sourceLocale: 'nl-NL',
-  pseudoLocale: 'dev',
-  fallbackLocales: {
-    default: 'nl-NL',
-  },
-  catalogs: [
-    {
-      path: '<rootDir>/src/locales/{locale}/messages',
-      include: ['<rootDir>/src'],
-      exclude: ['**/node_modules/**'],
+/**
+ * Configuration object for the module.
+ * @type {{
+ *   config: import('@lingui/conf').LinguiConfig,
+ *   localeList: Array<{title: string, value: string}>
+ * }}
+ */
+const moduleConfig = {
+  config: {
+    locales,
+    sourceLocale: 'nl-NL',
+    pseudoLocale: 'dev',
+    fallbackLocales: {
+      default: 'nl-NL',
     },
-  ],
-  format: 'po',
+    catalogs: [
+      {
+        path: '<rootDir>/src/locales/{locale}/messages',
+        include: ['<rootDir>/src'],
+        exclude: ['**/node_modules/**'],
+      },
+    ],
+    format: 'po',
+  },
+  localeList,
 };
+
+module.exports = moduleConfig;
